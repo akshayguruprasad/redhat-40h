df8c8bbe765c1f7b2ca3371b66e4c7b1fe18be73
G <akshay.g@capgemini.com>
2018-12-12 20:41:24 +0530

    first files shell

---
+++ Infrastructure/bin/setup_jenkins.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Setup Jenkins Project
+if [ "$#" -ne 3 ]; then
+    echo "Usage:"
+    echo "  $0 GUID REPO CLUSTER"
+    echo "  Example: $0 wkha https://github.com/redhat-gpte-devopsautomation/advdev_homework_template.git na311.openshift.opentlc.com"
+    exit 1
+fi
+
+GUID=$1
+REPO=$2
+CLUSTER=$3
+echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
+
+# Set up Jenkins with sufficient resources
+# TBD
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi
+
+# Create custom agent container image with skopeo
+# TBD
+  oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
+          USER root\nRUN yum -y install skopeo && yum clean all\n
+          USER akshay.g-capgemini.com' --name=jenkins-agent-appdev -n 7a9c-jenkins1
+
+# Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
+# TBD
+
+
+           oc new-app redhat-openjdk18-openshift~${REPO} --context-dir=openshift-tasks/ --build-env='REPO=https://github.com/redhat-gpte-devopsautomation/advdev_homework_template.git'
+--build-env='GUID=9e16' --build-env='CLUSTER=na311.openshift.opentlc.com'
+
+
+# Make sure that Jenkins is fully up and running before proceeding!
+while : ; do
+  echo "Checking if Jenkins is Ready..."
+  AVAILABLE_REPLICAS=$(oc get dc jenkins -n ${GUID}-jenkins -o=jsonpath='{.status.availableReplicas}')
+  if [[ "$AVAILABLE_REPLICAS" == "1" ]]; then
+    echo "...Yes. Jenkins is ready."
+    break
+  fi
+  echo "...no. Sleeping 10 seconds."
+  sleep 10
+done
